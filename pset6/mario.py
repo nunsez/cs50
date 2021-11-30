25754984be101b468bc13139df0bdaf8302aed7d
Alexander Mandrikov <mad.nunsez@gmail.com>
2021-11-30 10:39:24 +0000

    add mario

---
+++ pset6/mario.py
@@ -0,0 +1,28 @@
+def main():
+    height = get_height()
+
+    for floor in range(1, height + 1):
+        draw(floor, height)
+
+
+def get_height():
+    while True:
+        try:
+            height = int(input('Height: '))
+
+            if height > 0 and height < 9:
+                return height
+
+        except ValueError:
+            None
+
+
+def draw(floor, height):
+    offset = ' ' * (height - floor)
+    sharps = '#' * floor
+    line = f'{offset}{sharps}  {sharps}'
+    print(line)
+
+
+if __name__ == '__main__':
+    main()
